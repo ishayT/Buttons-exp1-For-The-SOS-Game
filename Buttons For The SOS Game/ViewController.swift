a5a9c3505fb6d3551eb0fef573df8f290cece9d6
Ishay <ishay@Ishays-Mac.local>
2018-03-14 21:15:02 +0200

    Initial Commit

---
+++ Buttons For The SOS Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buttons For The SOS Game
+//
+//  Created by Ishay on 3/14/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
